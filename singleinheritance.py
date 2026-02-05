9e19a45c3e1750632977039c9c7d08a6c6c95a2b
manju2341 <aidusiddaraju@gmail.com>
2026-02-05 12:14:30 +0530

    book issued

---
+++ singleinheritance.py
@@ -0,0 +1,24 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+    def display_book_details(self):
+        print("Title:", self.title)
+        print("Author:", self.author)
+class IssuedBook(Book):
+    def __init__(self, title, author, issued_to, issued_date):
+        super().__init__(title, author)
+        self.issued_to = issued_to
+        self.issued_date = issued_date
+
+    def display_details(self):
+        self.display_book_details()   
+        print("Issued To:", self.issued_to)
+        print("Issued Date:", self.issued_date)
+book1 = IssuedBook(
+    "Python Programming",
+    "xyz",
+    "Manjushree",
+    "03-02-2026"
+)
+book1.display_details()
