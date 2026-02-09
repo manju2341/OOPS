130294158adb1893fade090eb6bfd945324acedd
manju2341 <aidusiddaraju@gmail.com>
2026-02-09 11:47:32 +0530

    Vehicle Abstraction

---
+++ vehicl_Abstract.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+    @abstractmethod
+    def start_engine(self):
+        pass
+
+class Car(Vehicle):
+    def start_engine(self):
+        print("Car engine started")
+
+class Bike(Vehicle):
+    def start_engine(self):
+        print("Bike engine started")
+
+class Bus(Vehicle):
+    def start_engine(self):
+        print("Bus engine started")
+
+car = Car()
+car.start_engine()
